efcf9a39fde0fe27a6e7bd07206097b971fb2ac3
Ratan Hussain <mdratanmia7395@gmail.com>
2023-11-18 14:10:31 +0300

    Project is completed

---
+++ index.js
@@ -0,0 +1,58 @@
+
+let attemt = 0;
+let won = 0;
+let los = 0;
+
+// find all element here
+let guessNumber = document.querySelector('#guessNumber')
+let btn = document.querySelector('button')
+let entered = document.querySelector('.entered')
+let randomNumber = entered.nextElementSibling;
+let reminAttempt = randomNumber.nextElementSibling;
+let result = document.createElement('p')
+let body = document.querySelector('.body')
+
+
+// Create a element with js
+result.classList.add('result')
+body.appendChild(result)
+
+
+// Lister
+btn.addEventListener('click',()=>{
+    event.preventDefault();
+    let inputed = guessNumber.value;
+    check(inputed)
+    attemt++;
+    reminAttempt.innerHTML = `Your remin attemt ${5 - attemt}`
+    if(attemt === 5){
+        btn.disabled = true;
+        guessNumber.disabled = true;
+    }else{
+    }
+
+})
+
+
+// checking match
+
+function  check(inputed){
+     let rand = randNo(5);
+     if(inputed == rand){
+         won++;
+        }else{
+            los++
+            entered.innerHTML = `You have enterd: ${inputed} Random number was: ${rand}`;
+        }
+        result.innerHTML = `Win :${won} & Lost :${los}`
+
+}
+
+
+
+
+// Random Number Creation
+function randNo(limit){
+    return Math.floor(Math.random() * limit )  + 1;
+
+}
